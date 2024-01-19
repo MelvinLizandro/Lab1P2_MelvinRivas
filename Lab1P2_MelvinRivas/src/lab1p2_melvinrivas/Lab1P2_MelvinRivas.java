437b1fa56857ca4301875fa74b67cf721531cbe1
MelvinLizandro <melvinrivas@unitec.edu>
2024-01-19 14:27:32 -0600

    2

--- Lab1P2_MelvinRivas/src/lab1p2_melvinrivas/Lab1P2_MelvinRivas.java
+++ Lab1P2_MelvinRivas/src/lab1p2_melvinrivas/Lab1P2_MelvinRivas.java
@@ -4,6 +4,15 @@
  */
 package lab1p2_melvinrivas;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
+import java.util.ArrayList;
+
+
 /**
  *
  * @author l3306
@@ -13,8 +22,62 @@ public class Lab1P2_MelvinRivas {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
         // TODO code application logic here
+        
+        Scanner input = new Scanner(System.in);
+        Pattern simbolos = Pattern.compile("^?=(!><$%)");
+        ArrayList<Regis> regi = new ArrayList<>();
+
+        //op = opciones
+        int op = 0;
+        do {
+            System.out.println("1. Registrar usuario");
+            System.out.println("2. Listar todo");
+            System.out.println("3. Listar por dominio");
+            System.out.println("4. Salir");
+
+            op = input.nextInt();
+
+            switch (op) {
+                case 1: {
+                    System.out.print("Ingrese su nombre: ");
+                    //nom = nombre
+                    String nom = input.nextLine();
+                    System.out.print("Ingrese su apellido: ");
+                    String apellido = input.nextLine();
+                    System.out.print("Ingrese su fecha ejemplo(10/12/2022): ");
+                    //fecha1 = lo pido como string para despues usar parse y Simpledate format para poder 
+                    //pasarlo date
+                    String fecha1 = input.nextLine();
+                    SimpleDateFormat ajusfec = new SimpleDateFormat("dd/MM/YYYY");
+                    Date fecha2 = ajusfec.parse(fecha1);
+                    System.out.print("Escriba su correo electronico: ");
+                    String correo = input.nextLine();
+                    Boolean aparece= Pattern.matches(simnbolos, correo);
+                    System.out.println("Ingrese su contraseña: ");
+                    //contra = contraseña
+                    String contra = input.nextLine();
+                }
+                case 2: {
+
+                    break;
+                }
+                case 3: {
+
+                    break;
+                }
+                case 4: {
+
+                    break;
+                }
+                default: {
+
+                    break;
+                }
+            }
+        } while (op != 4);
+
     }
-    
+
 }
